b671d844a0d5b46466e43bf5aeca52be454e2624
Candice-Chen <rmcandice@gmail.com>
2019-12-18 14:36:21 -0600

    added additional comments

--- A2G_G2S/PDB2A2G_G2S.py
+++ A2G_G2S/PDB2A2G_G2S.py
@@ -2,20 +2,25 @@ import os
 import subprocess
 infile = open("gmminput.txt","r")
 for line in infile:
+	#parse pdb id from input file
 	line = line.strip()
 	pdbid = line+".pdb"
 	try:
+		#download pdb file
 		command = "wget https://files.rcsb.org/download/"+pdbid
 		subprocess.check_call(command.split())
 		gmmoutput= line+"_100.gmm"
 		try:
+			#convert pdb to gaussion model then to surface model
 			os.system("./A2G_G2S.sh {0} {1}".format(pdbid,gmmoutput))
 			os.remove(pdbid)
 			os.remove("pdbfile0.pdb")
 			os.remove(gmmoutput)
 		except:
+			#raise error if program fail to convert pdb
 			f="error flag: fail to convert"+gmmoutput
 			print(f)
 	except:
+		#raise error if program fail to download pdb file
 		t="error flag: pdb not found" + line
 		print(t)
--- V2G_G2S/EMDB2V2G_G2S.py
+++ V2G_G2S/EMDB2V2G_G2S.py
@@ -2,10 +2,12 @@ import os
 import subprocess
 infile = open("gmminput.txt","r")
 for line in infile:
+	#parse emd id from input file
 	line = line.strip()
 	emdbid = line[4:]
 	emfile = "emd_"+line[4:] + ".map"
 	try:
+		#download emdb file and unzip file
 		command = "wget ftp://ftp.wwpdb.org/pub/emdb/structures/EMD-"+emdbid+"/map/emd_"+emdbid+".map.gz"
 		command2 = "gunzip "+emfile+".gz"
 		subprocess.check_call(command.split())
@@ -13,11 +15,14 @@ for line in infile:
 		gmmoutput= "emd_"+line[4:]+"_100.gmm"
 
 		try:
+			#convert em map to gaussion model, then to surface model
 			os.system("./V2G_G2S.sh {0} {1}".format(emfile,gmmoutput))
 			os.remove(emfile)
 			os.remove("pdbfile.pdb")
 			os.remove(gmmoutput)
 		except:
+			#raise error if fail to convert em map
 			print("error flag: fail to convert")
 	except:
+		#raise error if em file cannot be found
 		print("error flag: emdb not found")
